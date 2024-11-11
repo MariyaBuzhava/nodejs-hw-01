c4c8a77c8a0fb1a4824dffee8531554ff18af0ca
Mariya Buzhava <bujava.masha@gmail.com>
2024-11-11 22:31:40 +0200

    generateContacts.js

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,19 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  try {
+    const existingContacts = await readContacts();
+    const newContacts = Array.from({ length: number }, () =>
+      createFakeContact(),
+    );
+    const updatedContacts = [...existingContacts, ...newContacts];
+    await writeContacts(updatedContacts);
+    console.log(`${number} new contacts have been added successfully.`);
+  } catch (error) {
+    console.error('Error generating contacts:', error);
+  }
+};
 
 generateContacts(5);
